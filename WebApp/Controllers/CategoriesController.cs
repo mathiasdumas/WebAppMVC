2d7e14a9ef8dba8e5892cf7efdd2b22a71001a2e
mathias <mathiasdumaspro@gmail.com>
2024-04-10 22:17:21 +0200

    first commit

---
+++ WebApp/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApp.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var categories = CategoryRepository.GetCategories();
+            return View(categories);
+        }
+
+        public IActionResult Edit([FromRoute]int? id)
+        {
+            var category = CategoryRepository.GetCategoryById(id.HasValue?id.Value:0);
+            return View(category);
+        }
+    }
+}
+
---
+++ WebApp/Models/CategoryRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace WebApp.Models
+{
+	public static class CategoryRepository
+	{
+		private static List<Category> categories = new List<Category>()
+		{
+			new Category { CategoryId = 1, Name = "Beverate", Description = "Beverage" },
+			new Category { CategoryId = 2, Name = "Bakery", Description = "Bakery" },
+			new Category { CategoryId = 3, Name = "Meat", Description = "Meat" }
+		};
+
+		public static void AddCategory(Category category)
+		{
+			var maxId = categories.Max(x => x.CategoryId);
+			category.CategoryId = maxId + 1;
+			categories.Add(category);
+		}
+
+		public static List<Category> GetCategories() => categories;
+
+		public static Category? GetCategoryById(int categoryId)
+		{
+			var category = categories.FirstOrDefault(x => x.CategoryId == categoryId);
+			if (category != null)
+			{
+				return new Category
+				{
+					CategoryId = category.CategoryId,
+					Name = category.Name,
+					Description = category.Description,
+				};
+			}
+			return null; 
+		}
+
+		public static void UpdateCategory(int categoryId, Category category)
+		{
+			if (categoryId != category.CategoryId) return;
+
+			var categoryToUpdate = GetCategoryById(categoryId);
+			if (categoryToUpdate != null)
+			{
+				categoryToUpdate.Name = category.Name;
+				categoryToUpdate.Description = category.Description;
+			}
+		}
+
+		public static void DeleteCategory(int categoryId)
+		{
+			var category = categories.FirstOrDefault(x => x.CategoryId == categoryId);
+			if (category != null)
+			{
+				categories.Remove(category);
+			}
+		}
+	}
+}
+
